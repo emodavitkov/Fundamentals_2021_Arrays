4cc8e654316edca10a8d00860a5c5636327e4640
emodavitkov <emodavitkov@abv.bg>
2021-01-29 11:39:53 +0200

    1

--- DayofWeek/Program.cs
+++ DayofWeek/Program.cs
@@ -6,7 +6,28 @@ namespace DayofWeek
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] days = new string [] 
+            {
+                "Monday",
+                "Tuesday",
+                "Wednesday",
+                "Thursday",
+                "Friday",
+                "Saturday",
+                "Sunday"
+            };
+
+            int day = int.Parse(Console.ReadLine());
+
+            if (day <1 || day>days.Length)
+            {
+                Console.WriteLine("Invalid day!");
+            }
+            else
+            {
+                Console.WriteLine($"{days[day-1]}");
+            }
+            
         }
     }
 }
---
+++ PrintNumbersinReverseOrder/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+
+namespace PrintNumbersinReverseOrder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
